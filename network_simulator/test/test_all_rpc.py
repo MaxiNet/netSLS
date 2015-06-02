7bb085d457ba8e14a14b05a3afadb802e1d2ccad
Malte Splietker <maltespl@mail.upb.de>
2015-06-02 22:29:54 +0200

    Revised rpc test
    Signed-off-by: Malte Splietker <maltespl@mail.upb.de>

--- network_simulator/test/test_all_rpc.py
+++ network_simulator/test/test_all_rpc.py
@@ -1,13 +1,22 @@
 #! /usr/bin/python
+
+import requests
+import json
+import logging
+import logging.config
+logging.config.fileConfig("logging.cfg")
+
 import threading
+import unittest
 
 from tinyrpc.protocols.jsonrpc import JSONRPCProtocol
 from tinyrpc.transports.http import HttpPostClientTransport
 from tinyrpc import RPCClient
 import zmq
 
-HOST ="fgcn-of-20.cs.upb.de"
-HOST ="localhost"
+logger = logging.getLogger("Testing")
+
+HOST = "localhost"
 
 class Sender(threading.Thread):
     count = 0
@@ -32,69 +41,100 @@ class Sender(threading.Thread):
         self.destination = destination
         self.n_bytes = n_bytes
 
+        self.result = ""
+
     def run(self):
-        transmission_id = self.remote_server.transmit_n_bytes(
-            "", self.source, self.destination, self.n_bytes, self.key)
-        print("%s: Returned transmission_id = %i" % (self.key, transmission_id))
-        print("%s: [ZMQ] %s" % (self.key, self.subscriber.recv().splitlines()[1]))
+        try:
+            transmission_id = self.remote_server.transmit_n_bytes(
+                "", self.source, self.destination, self.n_bytes, self.key)
+        except requests.exceptions.ConnectionError:
+            self.result = "CONNECTION_REFUSED"
+            return
+        result = self.subscriber.recv().splitlines()[1]
+        logger.info("%s: Returned transmission_id = %i" % (self.key, transmission_id))
+        logger.debug("%s: [ZMQ] %s" % (self.key, result))
 
-def main():
-    context = zmq.Context()
-    subscriber = context.socket(zmq.SUB)
-    subscriber.connect("tcp://%s:5500" % HOST)
-    subscriber.setsockopt(zmq.SUBSCRIBE, "DEFAULT")
-    subscriber.setsockopt(zmq.SUBSCRIBE, "SUB_KEY")
+        self.result = json.loads(result)["type"]
 
-    rpc_client = RPCClient(
-        JSONRPCProtocol(),
-        HttpPostClientTransport("http://%s:5501" % HOST)
-    )
-    remote_server = rpc_client.get_proxy()
+class TestRPCFunctions(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.context = zmq.Context()
+        cls.subscriber = cls.context.socket(zmq.SUB)
+        cls.remote_server = None
 
-    racks = [ \
-        {"id":"rack0", "hosts":["host00", "host01"]}, \
-        {"id":"rack1", "hosts":["host10", "host11"]}, \
-        {"id":"rack2", "hosts":["host20", "host21"]}, \
-        {"id":"rack3", "hosts":["host30", "host31"]}, \
-        ]
+        cls.subscriber.connect("tcp://%s:5500" % HOST)
+        cls.subscriber.setsockopt(zmq.SUBSCRIBE, "DEFAULT")
+        cls.subscriber.setsockopt(zmq.SUBSCRIBE, "SUB_KEY")
 
-    topology = {
-            "type" : "FatTree",
-            "arguments" : {
-                "racks" : racks
-                }
+        rpc_client = RPCClient(
+            JSONRPCProtocol(),
+            HttpPostClientTransport("http://%s:5501" % HOST)
+        )
+        cls.remote_server = rpc_client.get_proxy()
+
+        # Cross-test variables
+        cls.coflow_id = ""
+
+    def test_1_start_simulation(self):
+        cls = self.__class__
+
+        topology = {
+            "type": "FatTree",
+            "arguments": {
+                "racks": [
+                    {"id": "rack0", "hosts": ["host00", "host01"]},
+                    {"id": "rack1", "hosts": ["host10", "host11"]},
+                    {"id": "rack2", "hosts": ["host20", "host21"]},
+                    {"id": "rack3", "hosts": ["host30", "host31"]},
+                ]
             }
-    
-    print("== 1. Testing start_simulation ==")
-    print(remote_server.start_simulation(topology))
-    print("[ZMQ] %s" % subscriber.recv().splitlines()[1])
+        }
+        try:
+            result = cls.remote_server.start_simulation(topology)
+        except requests.exceptions.ConnectionError:
+            self.assertTrue(False, msg="RPC connection refused")
+        logger.debug("[ZMQ] %s" % cls.subscriber.recv().splitlines()[1])
+        self.assertTrue(result)
 
-    print
+    def test_2_register_coflow(self):
+        #TODO: test coflow description, once specified
+        cls = self.__class__
 
-    print("== 2. Testing register_coflow ==")
-    #TODO: test coflow description, once specified
-    coflow_id = remote_server.register_coflow("coflow description")
-    print("Returned coflow_id = %s" % coflow_id)
+        try:
+            result = cls.remote_server.register_coflow("coflow description")
+        except requests.exceptions.ConnectionError:
+            self.assertTrue(False, msg="RPC connection refused")
+        logger.debug("Returned coflow_id = %s" % self.coflow_id)
+        cls.coflow_id = result
+        self.assertTrue(result.startswith("COFLOW-"))
 
-    print
+    def test_3_unregister_coflow(self):
+        #TODO: test coflow description, once specified
+        cls = self.__class__
 
-    print("== 3. Testing unregister_coflow ==")
-    #TODO: test coflow description, once specified
-    print(remote_server.unregister_coflow(coflow_id))
+        try:
+            result = cls.remote_server.unregister_coflow(cls.coflow_id)
+        except requests.exceptions.ConnectionError:
+            self.assertTrue(False, msg="RPC connection refused")
+        self.assertTrue(result)
 
-    print
+    def test_4_transmit_n_bytes(self):
+        cls = self.__class__
 
-    print("== 4. Testing transmit_n_bytes ==")
-    senders = list()
-    for i in range(0, 1):
-        senders.append(Sender(context, "host00", "host01", 2))# * 1024 * 1024))
-        senders.append(Sender(context, "host20", "host21", 2))# * 1024 * 1024))
+        senders = list()
+        for i in range(0, 2):
+            senders.append(Sender(cls.context, "host00", "host01", 2 * 1024 * 1024))
+            senders.append(Sender(cls.context, "host20", "host21", 2 * 1024 * 1024))
 
-    for sender in senders:
-        sender.start()
+        for sender in senders:
+            sender.start()
 
-    for sender in senders:
-        sender.join()
+        for sender in senders:
+            sender.join()
+            self.assertNotEqual(sender.result, "CONNECTION_REFUSED", msg="RPC connection refused")
+            self.assertEqual(sender.result, "TRANSMISSION_SUCCESSFUL")
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestRPCFunctions)
+    unittest.TextTestRunner(verbosity=2).run(suite)
