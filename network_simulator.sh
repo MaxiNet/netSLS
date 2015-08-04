894d0a5f67dac7d8c961f41a62bc6a4ce61b9a5d
Malte Splietker <maltespl@mail.upb.de>
2015-08-04 14:23:46 +0200

    Added start scripts for both sls and network simulator
    Signed-off-by: Malte Splietker <maltespl@mail.upb.de>

---
+++ network_simulator.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cd network_simulator
+
+env/bin/python main.py
---
+++ sls.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+function print_usage {
+  echo -e "usage: sls.sh TraceFile"
+  echo -e
+  echo -e "Starts SLS with the given trace file."
+}
+
+if [[ -z $1 ]]; then
+  print_usage
+  exit 1
+fi
+
+TRACE_FILE=$(realpath $1)
+
+if [[ ! -f ${TRACE_FILE} ]]; then
+  echo "File not found: ${TRACE_FILE}"
+  print_usage
+  exit 1
+fi
+
+cd sls
+
+OUTPUT_DIRECTORY="/tmp/netSLS/output"
+mkdir -p ${OUTPUT_DIRECTORY}
+
+ARGS="-inputsls ${TRACE_FILE}"
+ARGS+=" -output ${OUTPUT_DIRECTORY}"
+ARGS+=" -printsimulation"
+
+mvn exec:java -Dexec.args="${ARGS}"
